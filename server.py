ca9dd67c813b9fb82615b68830791131624d2a8a
siddant <siddantvardey@gmail.com>
2020-07-13 09:57:05 +0530

    my portfolio website

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_csv(data):
+    with open('.\database.csv', mode='a') as database:
+        email = data['e-mail']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+
+
+@app.route('/submit_email', methods=['POST', 'GET'])
+def submit_email():
+    if request.method =='POST' :
+        data = request.form.to_dict()
+        print(data)
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else :
+        print('OOPS ! SOMETHING WENT WRONG, Try Again !!!')
+
