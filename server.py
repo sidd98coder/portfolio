123044440b6e0f7ade81b35710dfda855d4d743f
siddant <siddantvardey@gmail.com>
2020-07-20 22:01:16 +0530

    upadating server

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import csv
-from password_checker import *
+#from password_checker import *
 #from hackernews_project import *
 #from bs4 import BeautifulSoup
 app = Flask(__name__)
